15f9d589bd49d9223dc6fdb8bf352cd597c2b951
Abhijeet Kumar <114794064+Abhi22114@users.noreply.github.com>
2024-02-07 21:00:02 +0530

    Add files via upload

---
+++ intoduction.r
@@ -0,0 +1,27 @@
+#Introduction to R Programming language
+
+#1->>R is an interpreted programming language (not compiled base --> in which whole program execute at the same time)
+# program compile line by-line
+
+#uses of R
+
+#1-->R is used Analysis the statistical information,graphical representation ,Data Modelling,reporting,data visualization,Data Science,Data Analytics
+#it has lot packages ,we can easily use  machine learning algorithm concepts in r data-sets
+
+#-->>R is the implementation of the S programming language,which is combined with lexical scoping semantics
+#S programming is a commercial product ,formerly sold by TIBCO Software
+
+#open source language ,easy to downloads ,easy to customize ,can modify easily,we can modify the packages in R
+
+<<<<#HISTORY#>>>
+#-->>>IN THE  YEAR OF 1991 Developed by Ross Ihaka and Robert gentleman in the university of Auckland ,New-Zealand
+#1993 introduced in public
+#Martin Macheler convinces Ross and Robert to use the GNU(general public Licenses) to use free software
+  
+  
+  
+  
+  
+  
+  
+  
