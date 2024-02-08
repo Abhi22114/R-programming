e12c45cc066b180ddf0d277b7838ef09dc54b83f
Abhijeet Kumar <114794064+Abhi22114@users.noreply.github.com>
2024-02-08 23:38:57 +0530

    Add files via upload

---
+++ Features.r
@@ -0,0 +1,17 @@
+#R is a comprehensive programming language  it provides support for both procedural and object-oriented programming language with generic function
+#it has variety of packages ,provides more then 15000 packages for R on online repositories like CRAN ,Bioconductor,GitHub
+#it has amazing graphical capabilities
+
+#it doesn't need a compiler to convert the code into a program
+#cross platform support:R is cross platform supportive that is it can run on any OS and in any software without any hassle(without any difficulty)
+
+#it is a platform independent language
+#it performs fast calculations ,we can perform variety of complex operations on vectors,arrays,data frames and other data object of varying sizes 
+#MLR package in r helps in Machine learning concepts
+#we can implement machine learning algorithm with R language .
+#we can create object function and packages in it,platform independent language
+
+
+#note  more than 2millions job openings for R programming worldwide. (finance industries,health,banking,manufacture and many more uses R) 
+
+#R has  packages to perform Xgboost,Xgboost is one of the best algorithms for kaggle competition q
