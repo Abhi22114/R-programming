fbe97f6ea996228eeff149d30363184cdbe4339c
Abhijeet Kumar <114794064+Abhi22114@users.noreply.github.com>
2024-02-08 23:39:43 +0530

    Add files via upload

---
+++ Demo_program.r
@@ -0,0 +1,26 @@
+a<-2
+b<-4
+print(a+b)
+
+c<-4+7 
+c
+a<-"hello world it ois programming in R"  #hello world program
+a
+
+#simple declaration
+print("Hello world")
+
+4+71
+
+45*23/21 # gives the value 49.28571
+
+#to get the sum of two number
+sum<-10+20
+sum
+print(sum) #this will also print the sum R
+typeof(sum)
+
+a<-c(2,3,4,5)
+a
+typeof(a)
+print(class(a))
