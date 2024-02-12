53503915b7e72b8b23b5b4710a014e5e5c4c12e2
Abhijeet Kumar <114794064+Abhi22114@users.noreply.github.com>
2024-02-13 00:10:41 +0530

    Update vectordatatypes.r

--- vectordatatypes.r
+++ vectordatatypes.r
@@ -56,6 +56,16 @@ length(fruits)
 
 
 
+##--->>>Sort a Vector
+To sort items in a vector alphabetically or numerically, use the sort() function:
+
+##--Example
+
+fruits <- c("banana", "apple", "orange", "mango", "lemon")
+numbers <- c(13, 3, 5, 7, 20, 2)
+
+sort(fruits)  # Sort a string
+sort(numbers) # Sort numbers
 
 
 
