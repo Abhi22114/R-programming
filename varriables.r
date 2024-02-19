75ae0184593dd1176a1be35dcf166675bf3c3147
Abhijeet Kumar <114794064+Abhi22114@users.noreply.github.com>
2024-02-19 23:09:46 +0530

    Add files via upload

---
+++ varriables.r
@@ -0,0 +1,16 @@
+#variables are used to store data with named location that your programs can manipulate
+#variables in R can be a combination of letters ,digits,period, and Underscore
+
+
+
+
+#valid variables   #invalid variables
+         
+total             #tit@l
+sum               #5um
+.count            #count
+.count.total       #TRUE
+Var                #.0ar
+
+"hello" -> var3
+var3
